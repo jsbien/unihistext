6b3bc2b9b8b26a47c2cc35c371749654ef8def9a
Piotr Findeisen <piotr.findeisen@gmail.com>
2008-12-12 00:33:47 +0100

    versioning more robust

--- src/version-gen.sh
+++ src/version-gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git describe 2>/dev/null | grep -qE . || exit 1
-ver=`git describe`
+git describe --always 2>/dev/null | grep -qE . || exit 1
+ver=`git describe --always`
 
 git update-index -q --refresh
 test -z "$(git diff-index --name-only HEAD --)" || ver="$ver-dirty"
